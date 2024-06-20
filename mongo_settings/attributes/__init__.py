74616353476cadddb34c55d90f794cd02cb452a6
bonifacius <become.lev.03@mail.ru>
2024-06-20 13:26:36 +0700

    Add: general code base

---
+++ mongo_settings/attributes/__init__.py
@@ -0,0 +1,1 @@
+from .from_mongo import FromMongo
---
+++ mongo_settings/mongo_settings.py
@@ -0,0 +1,56 @@
+from pydantic_settings import BaseSettings
+from cachetools import TTLCache
+from pymongo import MongoClient
+from pymongo.collection import Collection
+
+from .attributes import FromMongo
+
+
+class MongoEnvMixin:
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        m_con = self.MongoConnector
+        self._env_collection = self._get_env_collection(
+            db=m_con.db,
+            collection=m_con.collection,
+            creds=m_con.creds
+        )
+        self._cache = TTLCache(maxsize=64, ttl=120)
+
+    def __getattribute__(self, item):
+        field = super().__getattribute__(item)
+        if not isinstance(field, FromMongo):
+            return field
+        try:
+            return self._cache[item]
+        except KeyError:
+            value = field.get(self._env_collection, item)
+        if value is None:
+            raise AttributeError(
+                f'{type(self).__name__!r} object has no attribute {item!r}'
+            )
+        self._cache[item] = value
+        return value
+
+    @staticmethod
+    def _get_env_collection(db: str, collection: str, creds: BaseSettings) -> Collection:
+        client = MongoClient(
+            host=creds.host,
+            port=creds.port,
+            username=creds.username,
+            password=creds.password
+        )
+        db = client[db]
+        collection = db[collection]
+        return collection
+
+
+class FromMongoSettings(MongoEnvMixin, BaseSettings):
+    def __init__(self, *args, **kwargs):
+        try:
+            self.MongoConnector
+        except AttributeError:
+            raise AttributeError(
+                f"{type(self).__name__!r} object has not required attribute 'MongoConnector'"
+            )
+        super().__init__(*args, **kwargs)
